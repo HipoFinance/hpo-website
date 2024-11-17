83a25e481600119581a3903f98821a31dab8206f
Behrang Norouzinia <behrangn@gmail.com>
2024-11-17 10:30:28 +0330

    Add HPO website

---
+++ src/main.tsx
@@ -0,0 +1,47 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import '@fontsource/heebo/latin-400.css'
+import '@fontsource/heebo/latin-500.css'
+import '@fontsource/heebo/latin-700.css'
+import '@fontsource/eczar/latin-700.css'
+
+const aMinute = 1_000 * 60
+const anHour = aMinute * 60
+const aDay = anHour * 24
+
+updateRemainingTime()
+
+function updateRemainingTime() {
+    const now = Date.now()
+    const event = new Date(2024, 10, 25, 12, 0)
+    const remain = event.getTime() - now
+
+    if (remain < 0) {
+        return
+    }
+
+    setTimeout(updateRemainingTime, 5_000)
+
+    const days = Math.floor(remain / aDay)
+    const hours = Math.floor((remain % aDay) / anHour)
+    const minutes = Math.floor(((remain % aDay) % anHour) / aMinute)
+
+    const daysEl: HTMLElement | null = document.querySelector('#remainingDays')
+    const hoursEl: HTMLElement | null = document.querySelector('#remainingHours')
+    const minutesEl: HTMLElement | null = document.querySelector('#remainingMinutes')
+    if (daysEl == null || hoursEl == null || minutesEl == null) {
+        return
+    }
+
+    daysEl.innerText = days.toString()
+    hoursEl.innerText = hours.toString()
+    minutesEl.innerText = minutes.toString()
+}
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+)
---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            heebo: ['Heebo', ...fontFamily.sans],
+            eczar: ['Eczar'],
+        },
+        extend: {
+            colors: {
+                black: '#03010d',
+                white: '#ffffff',
+                purple1: '#6d1eb3',
+                purple2: '#461472',
+                purple3: '#dfbcfe',
+                pink1: '#e622c2',
+                darkblue1: '#170c55',
+                darkblue2: '#03010d',
+                gray1: '#dedbe4',
+            },
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                brush: 'url(brush.svg)',
+            },
+        },
+    },
+    plugins: [],
+} satisfies Config
